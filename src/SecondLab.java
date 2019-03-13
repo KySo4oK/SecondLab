0884feee8db044da1e7d6838e1638f87e905da11
Rostyslav <prostorostuk08@gmail.com>
2019-03-13 09:23:49 +0200

    First part of code

---
+++ src/SecondLab.java
@@ -0,0 +1,36 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SecondLab {
+    private static ArrayList<String> eurovision = new ArrayList();
+    private static int numberOfCountries;
+
+
+    public static void main(String[] args) {
+   //     int[][] votes = fillList();
+        //int[] result = votesToResult(votes);
+        //findWinnersAndPrint(result);
+    }
+
+//    public static int[][] fillList() {// method for filling votes
+//       ArrayList<String> lines = readAndSplitToLines();
+//        int[][] votes = new int[numberOfCountries][numberOfCountries];
+//        String[] wordsForCurrLine;
+//        for (int i = 0; i < lines.size(); i++) {
+//           wordsForCurrLine = lines.get(i).split(",");
+//            eurovision.add(wordsForCurrLine[0]);
+//            for (int j = 1; j < wordsForCurrLine.length; j++) {
+//                votes[i][j - 1] = Integer.parseInt(wordsForCurrLine[j]);
+//            }
+//        }
+//        return votes;
+//
+//    }
+}
